f57b7736c48cbdc4297cff3f01d2d9af5e2f9f2b
ascendzor <wow.troymare@gmail.com>
2016-04-13 20:57:21 +1200

    swagilicious

--- camera.js
+++ camera.js
@@ -1,15 +1,17 @@
 var setupCamera = function(scene) {
 	var camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.set(55, 60, 50);
-	camera.lookAt(new THREE.Vector3(55, 0, -25));
-	camera.up = new THREE.Vector3(0, 0, 1);
+	camera.position.set(0, 100, 0);
+	camera.lookAt(new THREE.Vector3(50, 0, -50));
+	camera.up = new THREE.Vector3(0, 1, 0);
 	scene.add( camera );
 
 	var someNumber = 0
 	setInterval(function() {
-		someNumber += 0.01
-		camera.position.z = Math.sin(someNumber) + 50
-	}, 10)
+		someNumber += 0.0005
+		camera.lookAt(new THREE.Vector3(50, 0, -50));
+		camera.position.z = Math.sin(someNumber) * -100
+		camera.position.x = Math.cos(someNumber) * 100
+	}, 16)
 
 	return camera;
 };
